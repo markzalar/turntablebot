4ceab801fc997d53f1c608936772e340960f3079
Mark Zalar <mark.zalar@gmail.com>
2012-06-09 18:35:37 -0400

    Fix issue with example command returns true

--- bot.js
+++ bot.js
@@ -60,7 +60,7 @@ function example(query) {
   var response = urban(query);
   response.first( function(json) {
     if(json.example && query.length > 0){
-      bot.speak(json.example && query.length > 0);
+      bot.speak(json.example);
     }
     else {
       bot.speak("I don't have an example of that.");
